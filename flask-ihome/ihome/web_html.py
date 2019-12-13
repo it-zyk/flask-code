c2952351070b1cc9bf7d4424fe0193f7adc36b41
it-zyk <it-zyk@outlook.com>
2019-12-14 00:29:46 +0800

    ihome project 正则

--- flask-ihome/ihome/web_html.py
+++ flask-ihome/ihome/web_html.py
@@ -10,24 +10,26 @@ html = Blueprint("web_html", __name__)
 # 127.0.0.1:5000/index.html
 # 127.0.0.1:5000/register.html
 
+
 @html.route("/<re(r'.*'):html_file_name>")
 def get_html(html_file_name):
     """提供html文件"""
-    # 如果 html_file_name 为"", 表示的路径是/，请求是主页
+    # 如果html_file_name为""， 表示访问的路径是/ ,请求的是主页
     if not html_file_name:
         html_file_name = "index.html"
 
-    # 如果资源名不是favicon.ico":
+    # 如果资源名不是favicon.ico
     if html_file_name != "favicon.ico":
         html_file_name = "html/" + html_file_name
 
     # 创建一个csrf_token值
     csrf_token = csrf.generate_csrf()
 
-    # flask 提供返回静态文件的方法
+    # flask提供的返回静态文件的方法
     resp = make_response(current_app.send_static_file(html_file_name))
 
-    # 设置cookie值 关闭浏览器重新生成
+    # 设置cookie值
     resp.set_cookie("csrf_token", csrf_token)
 
     return resp
+ 
