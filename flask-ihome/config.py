2693d312c82b77f96847a6e5e08584774e8074ea
it-zyk <it-zyk@outlook.com>
2019-12-13 21:45:55 +0800

    ihome project 配置

---
+++ flask-ihome/config.py
@@ -0,0 +1,36 @@
+import redis
+
+
+class Config(object):
+    """配置信息"""
+
+    SECRET_KEY = "KAEJF34KAK12344DFJAI"
+
+    # 数据库
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@127.0.0.1:3306/ihome_python04"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # redis
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # falsk-session配置
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    SESSION_USE_SIGNER = True   # 对cookie 中session_id 进行隐藏处理
+    PERMANENT_SESSION_LIFETIME = 86400  # session 数据的有效期，单位秒 1天
+
+
+class DevelopmentConfig(Config):
+    """"开发模式的配置信息"""
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """生产环境配置信息"""
+    pass
+
+
+config_map = {
+    "develop": DevelopmentConfig,
+    "product": ProductionConfig
+}
