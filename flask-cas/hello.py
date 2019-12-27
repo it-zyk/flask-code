ded4056e7a6b0d5398a558e69ad7d6a995843ca8
it-zyk <it-zyk@outlook.com>
2019-12-27 23:05:27 +0800

    Flask Cas

--- flask-cas/hello.py
+++ flask-cas/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cas import CAS
 from flask_cas import login_required
 from flask_cas import login
@@ -7,14 +7,15 @@ from flask_cas import logout
 app = Flask(__name__)
 cas = CAS(app, '/cas')
 app.config['CAS_SERVER'] = 'https://cas.china-tcc.com'
-app.config['CAS_AFTER_LOGIN'] = 'route_root'
+app.config['CAS_AFTER_LOGIN'] = 'root'
 
 app.config['SECRET_KEY'] = "KAEJF34KAK12344DFJAI"
+
 
 @app.route('/')
 @login_required
 def route_root():
-    return flask.render_template(
+    return render_template(
         'layout.html',
         username=cas.username
     )
