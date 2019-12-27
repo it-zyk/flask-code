92b2ea15a9d5472d8e784f906ed457e85527a9a3
it-zyk <it-zyk@outlook.com>
2019-12-27 20:19:41 +0800

    flask web cas

--- flask-cas/hello.py
+++ flask-cas/hello.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_cas import CAS
 from flask_cas import login_required
+from flask_cas import login
+from flask_cas import logout
 
 app = Flask(__name__)
 cas = CAS(app, '/cas')
 app.config['CAS_SERVER'] = 'https://cas.china-tcc.com'
 app.config['CAS_AFTER_LOGIN'] = 'route_root'
 
+app.config['SECRET_KEY'] = "KAEJF34KAK12344DFJAI"
 
 @app.route('/')
 @login_required
