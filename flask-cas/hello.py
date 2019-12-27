23aa7f01cf184d62ad82a78af368c56a70509d5e
it-zyk <it-zyk@outlook.com>
2019-12-27 19:59:46 +0800

    flask web cas

--- flask-cas/hello.py
+++ flask-cas/hello.py
@@ -1,6 +1,20 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_cas import CAS
+from flask_cas import login_required
 
+app = Flask(__name__)
+cas = CAS(app, '/cas')
+app.config['CAS_SERVER'] = 'https://cas.china-tcc.com'
+app.config['CAS_AFTER_LOGIN'] = 'route_root'
+
+@app.route('/')
+@login_required
+def route_root():
+    return flask.render_template(
+        'layout.html',
+        username = cas.username,
+        display_name = cas.attributes['cas:displayName']
+    )
 
 @app.route('/')
 def index():
