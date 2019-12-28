2775a80aeb9b30ca9832409e63563fdb0336b2b1
it-zyk <it-zyk@outlook.com>
2019-12-28 21:13:58 +0800

    flask exam

--- flask-sesapp/sesapp/exam_v_1/demo.py
+++ flask-sesapp/sesapp/exam_v_1/demo.py
@@ -5,13 +5,13 @@ from flask import g, current_app, render_template
 from sesapp.utils.commons import login_required
 
 
-@login_required
 @exam.route("/index")
+@login_required
 def index():
     # current_app.logger.error("error msg")
     # current_app.logger.warn("warn msg")
     # current_app.logger.warn("warn msg")
-    # current_app.logger.info("info msg")
+    # current_app.logger.infso("info msg")
     # current_app.logger.debug("debug msg")
     return render_template(
         'layout.html',
